c93335725357e1f03807bb052d4fde32f6b81333
naichilab <naichilab@live.jp>
2023-06-14 01:15:34 +0900

    C#8で使えないメソッドを削除

--- Assets/unityroom/Api/Internals/UnixTime.cs
+++ Assets/unityroom/Api/Internals/UnixTime.cs
@@ -4,13 +4,22 @@ namespace unityroom.Api.Internals
 {
     internal static class UnixTime
     {
+        private static readonly DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
+
         /// <summary>
         /// 現在時刻のUnixTimeを返す
         /// </summary>
         /// <returns></returns>
         internal static int GetCurrentUnixTime()
         {
-            return (int)(DateTime.UtcNow.Subtract(DateTime.UnixEpoch)).TotalSeconds;
+            return ToUnixTime(DateTime.Now);
+        }
+
+        private static int ToUnixTime(DateTime targetTime)
+        {
+            targetTime = targetTime.ToUniversalTime();
+            var elapsedTime = targetTime - UnixEpoch;
+            return (int)elapsedTime.TotalSeconds;
         }
     }
 }
